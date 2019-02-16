ec02a9803760905c8b7b7c0b18a99e408f36edb5
Filip Jaskólski <f.jaskolski@livechatinc.com>
2019-02-16 01:13:33 +0100

    key

--- src/MessageList.js
+++ src/MessageList.js
@@ -4,11 +4,12 @@ import MessageListItem from "./MessageListItem";
 class MessageList extends React.PureComponent {
   render() {
     const { messages } = this.props;
+    console.log(JSON.stringify(messages, null, 2));
     return (
       <div className="messageList">
         <h3>Messages</h3>
-        {messages.map(group => (
-          <div className="messageGroup">
+        {messages.map((group, index) => (
+          <div className="messageGroup" key={index}>
             {group.map(messageId => (
               <MessageListItem key={messageId} id={messageId} />
             ))}
